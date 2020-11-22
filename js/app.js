f489ba693ac1648934b0e7d936453c7315e20234
activenoob <yusufkehinde11@gmail.com>
2020-11-22 21:49:45 +0100

    project is 98% complete

--- js/app.js
+++ js/app.js
@@ -1,49 +1,6 @@
-console.log("start");
-
-//STICKY NAVBAR ALTERNATIVE
-
-// window.onscroll = function () { myFunction() };
-
-// // Get the navbar
-// var navbar = document.getElementById("navbar");
-
-// // Get the offset position of the navbar
-// var sticky = navbar.offsetTop;
-
-// // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-// function myFunction() {
-//     if (window.pageYOffset >= sticky) {
-//         navbar.classList.add("sticky")
-//     } else {
-//         navbar.classList.remove("sticky");
-//     }
-// }
-
-// {
-//     viewer {
-//       avatarUrl
-//       login
-//       name
-//       repositories(last: 20, isFork: false) {
-//         nodes {
-//           name
-//           description
-//           url
-//           updatedAt
-//           forkCount
-// languages(orderBy: {field: SIZE, direction: DESC}, first: 1) {
-//   nodes {
-//     color
-//     name
-//   }
-// }
-//         }
-//       }
-//     }
-//   }
-
-// 4da385fc4fb0f7910ae879bb5d7f91ff65780568
-
+const misc = window.atob(
+  "ZWZiM2RmMzRlMGZiZWZmMDczZGE0MDQ4M2JlYmEwOGFmNGM1NTM5NA=="
+);
 const queryFetch = (query) => {
   return fetch("https://api.github.com/graphql", {
     method: "POST",
@@ -52,7 +9,7 @@ const queryFetch = (query) => {
     referrerPolicy: "no-referrer",
     headers: {
       "Content-Type": "application/json",
-      authorization: `token eb9b02065a5a403c9e0352541235e9bec73d5032`,
+      authorization: `token ${misc}`,
     },
     body: JSON.stringify({
       query: query,
@@ -73,49 +30,20 @@ queryFetch(
       
       
   }`
-).then((data) => {
-  document.getElementById("top_infoid").innerHTML = `
-           
-
-              <div  class="top_info">
-                <div class="user_img">
-                   <img src="${data.data.viewer.avatarUrl}" alt="avartar">
-               </div>
-               <div class="user_info">
-                  <h2>${data.data.viewer.name}</h2>
-                  <p>${data.data.viewer.login}</p>
-                </div>
-             </div>
-
-            <div class="setstatus">
-                <svg class="octicon octicon-smiley" viewBox="0 0 16 16" version="1.1" width="16" height="16"
-                    aria-hidden="true">
-                    <path fill-rule="evenodd"
-                        d="M1.5 8a6.5 6.5 0 1113 0 6.5 6.5 0 01-13 0zM8 0a8 8 0 100 16A8 8 0 008 0zM5 8a1 1 0 100-2 1 1 0 000 2zm7-1a1 1 0 11-2 0 1 1 0 012 0zM5.32 9.636a.75.75 0 011.038.175l.007.009c.103.118.22.222.35.31.264.178.683.37 1.285.37.602 0 1.02-.192 1.285-.371.13-.088.247-.192.35-.31l.007-.008a.75.75 0 111.222.87l-.614-.431c.614.43.614.431.613.431v.001l-.001.002-.002.003-.005.007-.014.019a1.984 1.984 0 01-.184.213c-.16.166-.338.316-.53.445-.63.418-1.37.638-2.127.629-.946 0-1.652-.308-2.126-.63a3.32 3.32 0 01-.715-.657l-.014-.02-.005-.006-.002-.003v-.002h-.001l.613-.432-.614.43a.75.75 0 01.183-1.044h.001z">
-                    </path>
-                </svg>
-
-                <span>Set status</span>
-            </div>
-
-            <div class="editprofile">
-                <button>Edit profile</button>
-            </div>
-
-            <div class="website">
-                <svg class="octicon octicon-link" viewBox="0 0 16 16" version="1.1" width="16" height="16"
-                    aria-hidden="true">
-                    <path fill-rule="evenodd"
-                        d="M7.775 3.275a.75.75 0 001.06 1.06l1.25-1.25a2 2 0 112.83 2.83l-2.5 2.5a2 2 0 01-2.83 0 .75.75 0 00-1.06 1.06 3.5 3.5 0 004.95 0l2.5-2.5a3.5 3.5 0 00-4.95-4.95l-1.25 1.25zm-4.69 9.64a2 2 0 010-2.83l2.5-2.5a2 2 0 012.83 0 .75.75 0 001.06-1.06 3.5 3.5 0 00-4.95 0l-2.5 2.5a3.5 3.5 0 004.95 4.95l1.25-1.25a.75.75 0 00-1.06-1.06l-1.25 1.25a2 2 0 01-2.83 0z">
-                    </path>
-                </svg>
-
-                <a class="website_link " href="${data.data.viewer.websiteUrl}">${data.data.viewer.websiteUrl}
-                </a>
-            </div>
-
-`;
-});
+)
+  .then((data) => {
+    const userDetails = data.data.viewer;
+    return userDetails;
+  })
+  .then((userDetails) => {
+    document.getElementById("top_infoid").innerHTML += `${userTemplate(
+      userDetails.avatarUrl,
+      userDetails.name,
+      userDetails.login,
+      userDetails.websiteUrl
+    )}`;
+    sticknav();
+  });
 
 let test = "";
 
@@ -149,7 +77,6 @@ queryFetch(
     return myRepos;
   })
   .then((myRepos) => {
-    console.log(myRepos);
     document.getElementById("repolist").innerHTML = `
   ${myRepos
     .map((myRepo) =>
@@ -166,21 +93,70 @@ queryFetch(
  `;
   });
 
-// myRepos.map(myRepo => {
-//   // console.log(myRepo.name);
-//   // console.log(myRepo.description);
-//   // console.log(myRepo.updatedAt);
-//   // console.log(myRepo.url);
+//UI TEMPLATES
+function userTemplate(avatarUrl, name, login, websiteUrl) {
+  return `
+      
+      <h3 id="testheader" class='headertest'>
+      <span class="small-img">
 
-//   // myRepo.languages.nodes.map(language => {
-//   //   console.log(language.color);
-//   //   console.log(language.name);
-//   // })
+          <img src="./img/small.jpeg" alt="">
+      </span>
+      <span class="small-name">codingmage</span>
+  </h3>
+ 
+    <div  class="top_info">
+      <div class="user_img">
+         <img src="${avatarUrl}" alt="avartar">
 
-//   repoTemplate(myRepo.name, myRepo.description, myRepo.updatedAt, myRepo.url, myRepo.languages.nodes)
-//   // repoTemplate()
+         <div class="status-anime">
+         <div class="emoji-container">
+             <svg class="octicon octicon-smiley" viewBox="0 0 16 16" version="1.1" width="16" height="16"
+                 aria-hidden="true">
+                 <path fill-rule="evenodd"
+                     d="M1.5 8a6.5 6.5 0 1113 0 6.5 6.5 0 01-13 0zM8 0a8 8 0 100 16A8 8 0 008 0zM5 8a1 1 0 100-2 1 1 0 000 2zm7-1a1 1 0 11-2 0 1 1 0 012 0zM5.32 9.636a.75.75 0 011.038.175l.007.009c.103.118.22.222.35.31.264.178.683.37 1.285.37.602 0 1.02-.192 1.285-.371.13-.088.247-.192.35-.31l.007-.008a.75.75 0 111.222.87l-.614-.431c.614.43.614.431.613.431v.001l-.001.002-.002.003-.005.007-.014.019a1.984 1.984 0 01-.184.213c-.16.166-.338.316-.53.445-.63.418-1.37.638-2.127.629-.946 0-1.652-.308-2.126-.63a3.32 3.32 0 01-.715-.657l-.014-.02-.005-.006-.002-.003v-.002h-.001l.613-.432-.614.43a.75.75 0 01.183-1.044h.001z">
+                 </path>
+             </svg>
 
-// })
+         </div>
+         <span>Set Status</span>
+     </div>
+     </div>
+     <div id="userinfo" class="user_info">
+        <h2>${name}</h2>
+        <p >${login}</p>
+      </div>
+   </div>
+
+  <div class="setstatus">
+      <svg class="octicon octicon-smiley" viewBox="0 0 16 16" version="1.1" width="16" height="16"
+          aria-hidden="true">
+          <path fill-rule="evenodd"
+              d="M1.5 8a6.5 6.5 0 1113 0 6.5 6.5 0 01-13 0zM8 0a8 8 0 100 16A8 8 0 008 0zM5 8a1 1 0 100-2 1 1 0 000 2zm7-1a1 1 0 11-2 0 1 1 0 012 0zM5.32 9.636a.75.75 0 011.038.175l.007.009c.103.118.22.222.35.31.264.178.683.37 1.285.37.602 0 1.02-.192 1.285-.371.13-.088.247-.192.35-.31l.007-.008a.75.75 0 111.222.87l-.614-.431c.614.43.614.431.613.431v.001l-.001.002-.002.003-.005.007-.014.019a1.984 1.984 0 01-.184.213c-.16.166-.338.316-.53.445-.63.418-1.37.638-2.127.629-.946 0-1.652-.308-2.126-.63a3.32 3.32 0 01-.715-.657l-.014-.02-.005-.006-.002-.003v-.002h-.001l.613-.432-.614.43a.75.75 0 01.183-1.044h.001z">
+          </path>
+      </svg>
+
+      <span>Set status</span>
+  </div>
+
+  <div class="editprofile">
+      <button>Edit profile</button>
+  </div>
+
+  <div id="navbarlogin" class="website">
+      <svg class="octicon octicon-link" viewBox="0 0 16 16" version="1.1" width="16" height="16"
+          aria-hidden="true">
+          <path fill-rule="evenodd"
+              d="M7.775 3.275a.75.75 0 001.06 1.06l1.25-1.25a2 2 0 112.83 2.83l-2.5 2.5a2 2 0 01-2.83 0 .75.75 0 00-1.06 1.06 3.5 3.5 0 004.95 0l2.5-2.5a3.5 3.5 0 00-4.95-4.95l-1.25 1.25zm-4.69 9.64a2 2 0 010-2.83l2.5-2.5a2 2 0 012.83 0 .75.75 0 001.06-1.06 3.5 3.5 0 00-4.95 0l-2.5 2.5a3.5 3.5 0 004.95 4.95l1.25-1.25a.75.75 0 00-1.06-1.06l-1.25 1.25a2 2 0 01-2.83 0z">
+          </path>
+      </svg>
+
+      <a class="website_link " href="${websiteUrl}">${websiteUrl}
+      </a>
+  </div>
+      
+      `;
+}
 
 function repoTemplate(
   name,
@@ -196,7 +172,7 @@ function repoTemplate(
             <div class="repo-container_left">
                 <div class="repo-top">
                     <h3 class="repo-text">
-                        <a href="#">${name}</a>
+                        <a href="${url}">${name}</a>
                     </h3>
                 </div>
                 <div class="forkfrom">
@@ -237,7 +213,7 @@ function repoTemplate(
                       `
                         : ""
                     }
-                    updater on 10 days ago
+                    Updated on ${dateTimeFormat(updatedAt)}
                 </div>
             </div>
 
@@ -262,8 +238,47 @@ function repoTemplate(
             `;
 }
 
-function testtemp(name) {
-  return `
-  <h1>hello</h1>
-  `;
+//STICKY HEADER IMAGE AND LOGIN
+const sticknav = () => {
+  window.onscroll = function () {
+    myFunction();
+    myFunction2();
+  };
+
+  // Get the navbar
+  var navbarlogin = document.getElementById("navbarlogin");
+  var tret = document.querySelector(".headertest");
+  var eee = document.querySelector(".user_info");
+  var userinfo = document.getElementById("userinfo");
+
+  // Get the offset position of the navbar
+  var sticky = navbarlogin.offsetTop;
+  var userinfo = navbarlogin.offsetTop;
+
+  function myFunction2() {
+    if (window.pageYOffset >= userinfo) {
+      eee.classList.add("commot");
+    } else {
+    }
+  }
+
+  // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+  function myFunction() {
+    if (window.pageYOffset >= sticky) {
+      tret.classList.add("sticky");
+    } else {
+      tret.classList.remove("sticky");
+    }
+  }
+};
+
+//DATE FORMAT
+
+function dateTimeFormat(date) {
+  const d = new Date(date);
+  const year = new Intl.DateTimeFormat("en", { year: "numeric" }).format(d);
+  const month = new Intl.DateTimeFormat("en", { month: "short" }).format(d);
+  const day = new Intl.DateTimeFormat("en", { day: "2-digit" }).format(d);
+
+  return `${day} ${month} ${year}`;
 }
